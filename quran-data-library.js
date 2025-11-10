049b4975091a023c76811414c05e2418ce24c74a
nn4192766-max <nn4192766@gmail.com>
2025-11-10 14:19:14 +0300

    Fix fetch URL formatting in getQuranData function

--- quran-data-library.js
+++ quran-data-library.js
@@ -1,7 +1,8 @@
 // quran-data-library.js
 const getQuranData = async () => {
   try {
-    const response = await fetch("https://api.alquran.cloud/v1/quran/quran-uthmani");
+    const response = await fetch("https://api.alquran.cloud/v1/quran/quran-uthmani
+");
     const data = await response.json();
     return data.data.surahs; // يحتوي على 114 سورة
   } catch (error) {
